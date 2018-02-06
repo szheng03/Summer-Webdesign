2b734b7255e63725132e5b86bfbee86e24139e54
Summer <szheng03@risd.edu>
2018-02-06 16:55:29 -0500

    compilation

---
+++ SummerCompilation/Projects/Project3-4/sleep copy/6thpage.js
@@ -0,0 +1,16 @@
+function showThoughts() { 
+    document.getElementById("Thoughts").style.display = "inline"; 
+}
+
+
+setTimeout("showThoughts()", 15000);
+
+
+let playNow = function() {
+  let audio = new Audio('Turning in Bed Sound Effect1.mp3');
+  audio.play();
+}
+
+setTimeout(function() {
+  playNow();
+}, 0);
---
+++ SummerCompilation/Projects/Project3-4/sleep copy/beach.js
@@ -0,0 +1,126 @@
+
+
+// function showremember() { 
+//     document.getElementById("remember").style.display = "inline"; 
+// }
+
+// setTimeout("showremember()", 8000);
+
+// function showcold() { 
+//     document.getElementById("cold").style.display = "inline"; 
+// }
+
+// setTimeout("showcold()", 18000);
+
+
+// function showcrab() { 
+//     document.getElementById("crab").style.display = "inline"; 
+// }
+
+
+// setTimeout("showcrab()", 28000);
+
+// function showseagull() { 
+//     document.getElementById("seagull").style.display = "inline"; 
+// }
+
+
+// setTimeout("showseagull()", 38000);
+
+// function showrock() { 
+//     document.getElementById("rock").style.display = "inline"; 
+// }
+
+
+// setTimeout("showrock()", 48000);
+
+// function showsmoke() { 
+//     document.getElementById("smoke").style.display = "inline"; 
+// }
+
+
+// setTimeout("showsmoke()", 58000);
+
+// function showend() { 
+//     document.getElementById("end").style.display = "inline"; 
+// }
+
+// setTimeout("showend()", 68000);
+
+
+
+// $(document).ready(function() {
+
+//     setTimeout(function() {
+
+//         $(".missing").fadeOut(3000,"linear");
+
+//     },8000);
+
+// });
+
+
+
+// $(document).ready(function() {
+
+//     setTimeout(function() {
+
+//         $(".remember").fadeOut(4000,"linear");
+
+//     },16000);
+
+// });
+
+// $(document).ready(function() {
+
+//     setTimeout(function() {
+
+//         $(".cold").fadeOut(4000,"linear");
+
+//     },24000);
+
+// });
+
+// $(document).ready(function() {
+
+//     setTimeout(function() {
+
+//         $(".crab").fadeOut(4000,"linear");
+
+//     },32000);
+
+// });
+
+// $(document).ready(function() {
+
+//     setTimeout(function() {
+
+//         $(".seagull").fadeOut(4000,"linear");
+
+//     },40000);
+
+// });
+
+
+// $(document).ready(function() {
+
+//     setTimeout(function() {
+
+//         $(".rock").fadeOut(4000,"linear");
+
+//     },48000);
+
+// });
+
+
+// $(document).ready(function() {
+
+//     setTimeout(function() {
+
+//         $(".smoke").fadeOut(4000,"linear");
+
+//     },56000);
+
+// });
+
+
